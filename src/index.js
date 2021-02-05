09fe54f373cff444351e1ebd79a20af5039bd007
Mohit Tadhiyal <mohtadhi@wkmin9458057.global.publicisgroupe.net>
2021-02-06 01:45:27 +0530

    Changes for css

--- src/index.js
+++ src/index.js
@@ -2,24 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Components/Routes';
-import Btn from './Components/Button';
+import Btn from './Components/Button/index';
 import Navbar from './Components/Navbar';
-
 
 ReactDOM.render(
   <div>
-   
-    <Routes/>
-    <Btn name = "Personal Information"/>
-    <Btn name = "AboutUs"/>
-    <Btn name = "ContactUs"/>
-    <Navbar/>
-    
-    </div>,
+    <Routes />
+    <Btn label="AboutUs" isDisabled={true} type="warning"/>
+    <Btn label="ContactUs" isDisabled={true} type="danger"/>
+    <Navbar />
+  </div>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
