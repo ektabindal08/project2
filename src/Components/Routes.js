6b79c2bb47b0ae2ae7ae7de1735f6241d9aa1ddb
Mohit Tadhiyal <mohtadhi@wkmin9458057.global.publicisgroupe.net>
2021-02-10 00:29:23 +0530

    changes for Nav BAR

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Contact from './Contact';
 import About from './About';
+import Navbar from './Navbar/index';
 
 const Routes = () => {
     return (
         <BrowserRouter>
+        <Navbar />
             <Switch>
                 <Route exact path='/' component={About} />
                 <Route exact path='/Contact' component={Contact} />
